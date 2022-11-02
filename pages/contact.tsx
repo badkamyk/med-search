6975ff397b6fd79f9397036b31b53cde3345e763
badkamyk <slothmateusz@gmail.com>
2022-11-02 11:34:37 +0100

    fix: footer

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-gray-900 min-h-screen">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact
                     Us</h2>
--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,6 +1,6 @@
 export default function Pricing() {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-gray-900 min-h-screen">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed
